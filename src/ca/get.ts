87df95ed6466dd99772d9522691ecf09c955b775
onichandame <zxinmyth@gmail.com>
2020-04-20 04:24:27 +0000

    rm print and fixed clibs

--- src/ca/get.ts
+++ src/ca/get.ts
@@ -4,8 +4,6 @@ import { Channel } from './channel'
 export const get = async (pvname: string): ReturnType<Channel['get']> => {
   const channel = await connect(pvname)
   const value = await channel.get()
-  console.log('disconnecting')
   await channel.disconnect()
-  console.log('disconnected')
   return value
 }
